bf19538191d2965844b686cd50dc6e32d768af4c
Veselin Todorov <veselin@veselin.bg>
2012-01-30 21:51:33 +0200

    Idea.

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-var filter = function(func) {
-  return new Filter(func);
+var filter = function(obj, func) {
+  return new Filter(obj, func);
 };
 
-function Filter(func) {
-  func.__filters = { 
-    before: [], 
+function Filter(obj, func) {
+  var original = obj[func];
+  
+  obj[func] = function() {
+    
+    obj[func].__filters.before.forEach(function(fn) {
+      fn.apply(obj, arguments);
+    });
+    
+    original.apply(obj, arguments);
+    
+    obj[func].__filters.after.forEach(function(fn) {
+      fn.apply(obj, arguments);
+    });
+  };
+  
+  obj[func].__filters = {
+    before: [],
     after: []
   };
-  this.original = func;
-  func = function() {};
+  
+  obj[func].clear = function() {
+    delete obj[func].__filters;
+    obj[func] = original;
+  };
+  
+  this.target = obj[func];
 };
 
-Filter.prototype.before = function(func) {
+Filter.prototype.before = function(fn) {
+  this.target.__filters.before.push(fn);
   return this;
 };
 
-Filter.prototype.after = function(func) {
+Filter.prototype.after = function(fn) {
+  this.target.__filters.after.push(fn);
   return this;
 };
 
@@ -36,3 +58,8 @@ filter(foo, 'bar')
   .after(function() {
     console.log('after');
   });
+
+
+foo.bar('main');
+foo.bar.clear();
+foo.bar('second');
